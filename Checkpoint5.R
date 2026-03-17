8c085605c304486964b0ba51f7f063477dea4984
cjwdolly-droid <cjw0919dolly@gmail.com>
2026-03-17 15:46:30 -0400

    update

--- Checkpoint5.R
+++ Checkpoint5.R
@@ -1,1 +1,8 @@
-print("hello")
+# Source - https://stackoverflow.com/q/74187411
+# Posted by Karimic Magellan, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-03-17, License - CC BY-SA 4.0
+
+temp <- tempfile()
+download.file("https://health.data.ny.gov/download/tk4g-wdfe/application%2Fx-zip-compressed",temp, mode="wb")
+unzip(temp, "NYSDOH_BRFSS_SurveyData_2023.csv")
+db <- read.csv("NYSDOH_BRFSS_SurveyData_2023.csv", header=T)
